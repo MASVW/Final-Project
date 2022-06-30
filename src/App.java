49ff52956a2295e936785a9a9e846ad56fc578a4
Ca-ri-ssa <94051808+Ca-ri-ssa@users.noreply.github.com>
2022-06-30 12:37:12 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Halo Kelompok 2");
-        System.out.println("Carissa");
+        System.out.println("Halo, Carissa");
         System.out.println("halo-halo");
     }
 }
