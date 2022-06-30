8aa891eee6558f902fb03839222e4808a1153aca
Karen Onggie <106134898+kkrn38@users.noreply.github.com>
2022-06-30 12:32:59 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Halo Kelompok 2");
         System.out.println("Carissa");
+        System.out.println("halo-halo");
     }
 }
