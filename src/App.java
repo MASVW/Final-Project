c26b4dc268218bb2359cbae7665da79b651a52b3
Ca-ri-ssa <94051808+Ca-ri-ssa@users.noreply.github.com>
2022-06-30 12:31:09 +0700

    Create App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Halo Kelompok 2");
-        System.out.println("Halo-halo");
+        System.out.println("Carissa");
     }
 }
